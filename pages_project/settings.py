9bdece9d7fe8a5418b2fa7e8b53043d8bea39317
Jinson George <37330278+georgejinson@users.noreply.github.com>
2020-08-03 02:43:05 +0530

    Use env secret key for production

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!69vfd6gk%ll^cl$!r34-y_eqwjc45@c&0-spcd0own4r8^sq9'
+SECRET_KEY = env("DJANGO_SECRET_KEY", default='!69vfd6gk%ll^cl$!r34-y_eqwjc45@c&0-spcd0own4r8^sq9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
